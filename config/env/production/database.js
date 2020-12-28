c7fed8130269ce60f45d7e801bd7a3c2546fe550
Garbanv <garban.valdeon@gmail.com>
2020-12-28 17:03:01 +0100

    change on config

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -5,11 +5,11 @@ module.exports = ({ env }) => ({
         connector: 'bookshelf',
         settings: {
           client: 'postgres',
-          host: env('DATABASE_HOST', 'localhost'),
+          host: env('DATABASE_HOST', 'ec2-54-162-207-150.compute-1.amazonaws.com'),
           port: env.int('DATABASE_PORT', 5432),
-          database: env('DATABASE_NAME', 'platform'),
-          username: env('DATABASE_USERNAME', 'postgres'),
-          password: env('DATABASE_PASSWORD', '12345'),
+          database: env('DATABASE_NAME', 'd4fv6jn8l2j774'),
+          username: env('DATABASE_USERNAME', 'sldzkgpykknjre'),
+          password: env('DATABASE_PASSWORD', 'd204db97aba849172999c6f783e0dadf74804405e969a04e2d5c6c46b1991e85'),
           schema: env('DATABASE_SCHEMA', 'public'), // Not Required
           ssl: {
             rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
